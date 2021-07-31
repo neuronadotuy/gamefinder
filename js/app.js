59be4c414bebe1d4764dd1c0f30e30a663dbe59e
Nicolas <nico@neurona.uy>
2021-07-31 00:15:21 -0300

    login form functionality, showing error on inputs

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const inputName = document.querySelector('#input_name');
 const inputPassword = document.querySelector('#input_pass');
 const btnSubmit = document.querySelector('#btn_submit');
+const btnLogout = document.querySelector('#logout');
 
 const iconName = document.querySelector('#icon_name');
 const showOrHidePassword = document.querySelector('.show-hide__icon');
@@ -58,8 +59,6 @@ function inputNameError(e) {
 		nameError.classList.add('error_message');
 		nameError.textContent = 'Enter a valid email';
 		return;
-	} else {
-		console.log(inputName.value);
 	}
 }
 
@@ -115,22 +114,80 @@ function inputPassFocus(e) {
 	e.target.parentElement.removeChild(passError);
 }
 
-// start server
+// friendly reminder how to start the server
 // json-server db.json -m ./node_modules/json-server-auth
+
+//Regex
+const re =
+	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 async function login(e) {
 	e.preventDefault();
-	const email = inputName.value;
-	const password = inputPassword.value;
+	const email = inputName;
+	const password = inputPassword;
 	const server = 'http://localhost:3000/login';
 
-	const req = await fetch(server, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({ email: email, password: password }),
-	});
-	const res = await req.json();
-	console.log(res);
+	let emailValidation;
+	let passValidation;
+
+	try {
+		const req = await fetch(server, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ email: email.value, password: password.value }),
+		});
+		const res = await req.json();
+
+		// Username validation
+		if (!re.test(email.value.toLowerCase()) || email.value === 0) {
+			emailValidation = false;
+			email.classList.add('input_error', 'username_error');
+			email.parentElement.children[0].classList.add('icon_error');
+			email.parentElement.children[0].children[0].classList.add(
+				'icon-svg_error'
+			);
+
+			email.parentElement.appendChild(nameError);
+			nameError.classList.add('error_message');
+			nameError.textContent = 'Enter a valid email';
+			console.log(emailValidation);
+		} else {
+			emailValidation = true;
+			console.log(emailValidation);
+		}
+
+		// Username validation
+		if (password.value.trim().length <= 3) {
+			passValidation = false;
+			password.classList.add('input_error', 'username_error');
+			password.parentElement.children[0].classList.add('icon_error');
+			password.parentElement.children[0].children[0].classList.add(
+				'icon-svg_error'
+			);
+
+			password.parentElement.appendChild(passError);
+			passError.classList.add('error_message');
+			passError.textContent = 'Enter a valid email';
+			console.log(emailValidation);
+		} else {
+			passValidation = true;
+			console.log(emailValidation);
+		}
+
+		// localStorage.setItem('JWT', res.accessToken);
+		// window.location.href = 'http://localhost:5501/main.html';
+		console.log(res);
+	} catch (error) {
+		console.log(error);
+	}
 }
+
+// if (localStorage.getItem('JWT')) {
+// 	window.location.href = 'http://localhost:5501/main.html';
+// }
+
+const logout = () => {
+	localStorage.removeItem('JWT');
+};
